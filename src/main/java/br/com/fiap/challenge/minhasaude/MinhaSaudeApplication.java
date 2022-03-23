a0f48eea4bc2546f430becd7cd1ced29a12a237c
Felipe Ayoub Zanini <fayoub94@hotmail.com>
2022-03-23 09:07:38 -0300

    first commit

---
+++ src/main/java/br/com/fiap/challenge/minhasaude/MinhaSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.minhasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaSaudeApplication.class, args);
+	}
+
+}
